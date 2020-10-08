3facb8e34efb981a747b3a3e36e2e9b6eb505f42
surajsrv11 <surajvishwakarma625@gmail.com>
2020-10-08 17:34:32 +0530

    Updates

---
+++ precache-manifest.faa0c56150d050657c89fd7b5be112c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04749a267a71a964ca9bfe07b8a4ff7",
+    "url": "/get-weather/index.html"
+  },
+  {
+    "revision": "e7b30f9d2ff548374a1a",
+    "url": "/get-weather/static/css/main.4836dfbb.chunk.css"
+  },
+  {
+    "revision": "bfdd9448c98122f521c4",
+    "url": "/get-weather/static/js/2.cbcb37ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/get-weather/static/js/2.cbcb37ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b30f9d2ff548374a1a",
+    "url": "/get-weather/static/js/main.7d019218.chunk.js"
+  },
+  {
+    "revision": "c7e5ead1be429850bc00",
+    "url": "/get-weather/static/js/runtime-main.c1d40045.js"
+  }
+]);

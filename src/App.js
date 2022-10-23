74b297e29199609d4f8f4acb7cdb88cd6e8466fb
- <->
2022-10-23 19:26:11 +0500

    Loader will be shown when fetching data

--- src/App.js
+++ src/App.js
@@ -1,124 +1,138 @@
 import React from "react";
-import Appcontainer from "./Appcontainer.js"
+import Appcontainer from "./Appcontainer.js";
 import Toast from "./Toast.js";
-import "./index.css"
+import "./index.css";
 
 class App extends React.Component {
-  constructor(){
-      super()
-      {
-          this.state = {
-              city_name : "london",
-              error: false,
-              toast_message:'',
-              temp : [],
-              city : [],
-              icon : [],
-              desc : [],
-              humidity : [],
-              pressure : [],
-              wind : [],
-              visibility : []
-
-          }
-      }
-      this.city=this.city.bind(this)
-      this.submit=this.submit.bind(this)
-      this.componentWillMount=this.componentWillMount.bind(this)
-      this.closeToasterMessage = this.closeToasterMessage.bind(this)
-  }
-  
-
-  city(event){
-      const {value} = event.target;
-      this.setState({
-          city_name : value
-      }
-      )
-
+  constructor() {
+    super();
+    {
+      this.state = {
+        city_name: "london",
+        error: false,
+        toast_message: "",
+        temp: [],
+        city: [],
+        icon: [],
+        desc: [],
+        humidity: [],
+        pressure: [],
+        wind: [],
+        visibility: [],
+        displayLoader: "none",
+      };
+    }
+    this.city = this.city.bind(this);
+    this.submit = this.submit.bind(this);
+    this.componentWillMount = this.componentWillMount.bind(this);
+    this.closeToasterMessage = this.closeToasterMessage.bind(this);
   }
 
-  closeToasterMessage(){
-      // Set error status back to false
-      this.setState(prevState => {
-          return {
-                  ...prevState, 
-                  error: false,
-                  toast_message:''
-                }
-      })
+  city(event) {
+    const { value } = event.target;
+    this.setState({
+      city_name: value,
+    });
   }
 
-submit(e){
+  closeToasterMessage() {
+    // Set error status back to false
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        error: false,
+        toast_message: "",
+      };
+    });
+  }
+
+  submit(e) {
     e.preventDefault();
     this.componentWillMount();
-}
-  
-  componentWillMount(){
-      const city = this.state.city_name
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0861a5029ae242c98d1f8edcbf54215c`
-      fetch(url)
-      .then(response => {
-        // Check if the response was successful
-        if (!response.ok)
-        { 
-          throw Error(response.status)
-        }
-        return response.json()
-      })
-      .then(response => (
-          this.setState(
-              {
-                  temp : response.main.temp,
-                  city : response.name,
-                  icon : response.weather[0].icon,
-                  desc : response.weather[0].description,
-                  humidity : response.main.humidity,
-                  pressure : response.main.pressure,
-                  wind : response.wind.speed,
-                  visibility : response.visibility,
-                  error: false,
-                  toast_message: '' 
-              }
-          )
-      ))
-      .catch(error=>{
-          // Handle 404 error. Render toast message indicating location is invalid
-          if(error.message === '404')
-          { 
-              this.setState(prevState => {
-                  return {
-                         ...prevState, 
-                         error: true,
-                         toast_message: "The location entered is invalid. Please enter valid location." 
-                         }
-              })   
-          }            
-      })
   }
-  render(){
-    return(
-        <div>
-            <form className="container form">
-            <input className="input-box" name="t1" type="text" placeholder="Search location" onChange={this.city} placeholder="Enter desired location"></input>
-            <button className="submit-button" onClick={this.submit}><p className="p-submit">SUBMIT</p></button>
-            </form>
-            <Appcontainer 
-            temp = {this.state.temp}
-            city = {this.state.city}   
-            icon = {this.state.icon} 
-            desc = {this.state.desc}
-            humidity = {this.state.humidity}
-            pressure = {this.state.pressure}
-            wind = {this.state.wind}
-            visibility = {this.state.visibility}
-            />
-           {this.state.error && <Toast 
-           closeToasterMessage = {this.closeToasterMessage}
-           toasterMessage = {this.state.toast_message}
-           />}  
-        </div>
-        )
-    }
+
+  componentWillMount() {
+    document.body.style.filter = "blur(0px)"
+    // Show loader
+    this.setState({ displayLoader: "block" });
+    const city = this.state.city_name;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0861a5029ae242c98d1f8edcbf54215c`;
+    fetch(url)
+      .then((response) => {
+        // Hide Loader when get the response
+        this.setState({ displayLoader: "none" });
+        // Check if the response was successful
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .then((response) =>
+        this.setState({
+          temp: response.main.temp,
+          city: response.name,
+          icon: response.weather[0].icon,
+          desc: response.weather[0].description,
+          humidity: response.main.humidity,
+          pressure: response.main.pressure,
+          wind: response.wind.speed,
+          visibility: response.visibility,
+          error: false,
+          toast_message: "",
+        })
+      )
+      .catch((error) => {
+        // Handle 404 error. Render toast message indicating location is invalid
+        if (error.message === "404") {
+          this.setState((prevState) => {
+            return {
+              ...prevState,
+              error: true,
+              toast_message:
+                "The location entered is invalid. Please enter valid location.",
+            };
+          });
+        }
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div className="blur-wall"></div>
+        <div
+          className="loader"
+          style={{ display: this.state.displayLoader }}
+        ></div>
+        <form className="container form">
+          <input
+            className="input-box"
+            name="t1"
+            type="text"
+            onChange={this.city}
+            placeholder="Enter desired location"
+          ></input>
+          <button className="submit-button" onClick={this.submit}>
+            <p className="p-submit">SUBMIT</p>
+          </button>
+        </form>
+        <Appcontainer
+          temp={this.state.temp}
+          city={this.state.city}
+          icon={this.state.icon}
+          desc={this.state.desc}
+          humidity={this.state.humidity}
+          pressure={this.state.pressure}
+          wind={this.state.wind}
+          visibility={this.state.visibility}
+        />
+        {this.state.error && (
+          <Toast
+            closeToasterMessage={this.closeToasterMessage}
+            toasterMessage={this.state.toast_message}
+          />
+        )}
+      </div>
+    );
+  }
 }
-export default App
+export default App;

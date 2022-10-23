16c856e5c267f025a34a3b2d140239273af33a7b
Yuvraj Agarkar <yuvraj.agarkar@gmail.com>
2022-10-23 23:25:57 +0530

    added little animation and detailing

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./index.css";
 
-function Header(){
-    return(
+function Header() {
+  return (
     <header>
-        <div className="container ui block header">
-            <h1 className="tittle">GET WEATHER</h1>
-        </div>
+      <div className="container ui block header">
+        <h1 className="tittle">GET WEATHER 💨</h1>
+      </div>
     </header>
-    )
+  );
 }
 
-export default Header
+export default Header;

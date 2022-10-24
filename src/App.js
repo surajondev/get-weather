c098553eb3b19d0e256de48701d38a95f1b3d31f
Nachiketa Dhal <bnachiketa26@gmail.com>
2022-10-24 13:34:04 +0530

    Remove redundant block

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ import "./index.css"
 class App extends React.Component {
   constructor(){
       super()
-      {
-          this.state = {
-              city_name : "london",
-              error: false,
-              toast_message:'',
-              temp : [],
-              city : [],
-              icon : [],
-              desc : [],
-              humidity : [],
-              pressure : [],
-              wind : [],
-              visibility : []
-
-          }
+      this.state = {
+          city_name : "london",
+          error: false,
+          toast_message:'',
+          temp : [],
+          city : [],
+          icon : [],
+          desc : [],
+          humidity : [],
+          pressure : [],
+          wind : [],
+          visibility : []
+  
       }
       this.city=this.city.bind(this)
       this.submit=this.submit.bind(this)
       this.componentWillMount=this.componentWillMount.bind(this)
       this.closeToasterMessage = this.closeToasterMessage.bind(this)
-  }
+      }
   
 
   city(event){

73e4eac36c325384f5d2eba10ff5be9b8646da31
harikumar <harikumar3868@gmail.com>
2023-10-16 14:50:41 +0530

    feat: UI Updated

---
+++ src/ThemeContext.js
@@ -0,0 +1,22 @@
+// ThemeContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
